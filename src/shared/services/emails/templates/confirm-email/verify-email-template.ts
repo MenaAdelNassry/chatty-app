65ba9853eea83435fe9048673f2678a30cec1a8d
Mena Adel <f18408aa9@gmail.com>
2026-02-25 21:48:06 +0200

    handle sneding email problem

--- src/shared/services/emails/templates/confirm-email/verify-email-template.ts
+++ src/shared/services/emails/templates/confirm-email/verify-email-template.ts
@@ -7,6 +7,13 @@ class VerifyEmailTemplate {
   public emailVerificationTemplate(username: string, otp: string): string {
     const templatePath = path.join(__dirname, 'verify-email-template.ejs');
 
+    if (fs.existsSync(templatePath)) {
+      console.log('✅ Success: Template found at', templatePath);
+    } else {
+      console.log('❌ Error: Template NOT found at', templatePath);
+      console.log('📁 Files in this directory:', fs.readdirSync(__dirname));
+    }
+
     const image_url = 'https://res.cloudinary.com/dpjqyf1hm/image/upload/v1769613622/download_sbkend.png';
 
     return ejs.render(fs.readFileSync(templatePath, 'utf-8'), {
